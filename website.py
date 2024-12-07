f5ca8e24a1405c2deb0342fbda0c6aced427026b
luzieahrens <luzie.ahrens@gmail.com>
2024-12-07 18:09:43 +0100

    first commit

---
+++ website.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template  
+app = Flask(__name__)   # Flask constructor
+from datetime import datetime
+
+
+  
+# A decorator used to tell the application 
+# which URL is associated function 
+@app.route('/')       
+def hello():
+    today = datetime.today().date().day
+    song = 13%13 + 1
+    info = {"tag": today, "song": f"../static/audio/{song}.wav"}
+    return render_template("index.html", info=info)
+  
+if __name__=='__main__': 
+   app.run()
