93703f0ad240a57b8c0a053a1f3d2045b323c671
luzieahrens <luzie.ahrens@gmail.com>
2024-12-09 07:49:45 +0100

    change song choosing

--- website.py
+++ website.py
@@ -9,7 +9,7 @@ from datetime import datetime
 @app.route('/')       
 def hello():
     today = datetime.today().date().day
-    song = 13%13 + 1
+    song = today%13 + 1
     info = {"tag": today, "song": f"../static/audio/{song}.mp3"}
     return render_template("index.html", info=info)
   
